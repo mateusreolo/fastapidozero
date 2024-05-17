e698545e724bd37c057422d43722106a50c9557d
Mateus Reolo <mateus.bregantin.silva@oracle.com>
2024-05-17 18:06:53 -0300

    Implementacao de PUT

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -9,7 +9,17 @@ class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
-    
+
+
 class UserPublic(BaseModel):
+    id: int
     username: str
     email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
