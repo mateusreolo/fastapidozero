90ddd682d4a178febd1f9468d0634c8c601a17aa
Mateus Reolo <mateus.bregantin.silva@oracle.com>
2024-05-17 17:37:23 -0300

    POST e validação de email enviado

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK,response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
