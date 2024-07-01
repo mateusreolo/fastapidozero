352d29c384b3bd20cad9ab191981219121f71328
Mateus Reolo <mateus.bregantin.silva@oracle.com>
2024-07-01 11:28:52 -0300

    Atualizando endpoints para usar no banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -29,9 +31,13 @@ def test_create_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [{'username': 'alice', 'email': 'alice@example.com', 'id': 1}]
-    }
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users')
+    assert response.json() == {'users': [user_schema]}
 
 
 """
@@ -57,7 +63,7 @@ def test_read_user_not_found(client):
 """
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -74,7 +80,7 @@ def test_update_user(client):
     }
 
 
-def test_update_user_not_found(client):
+def test_update_user_not_found(client, user):
     response = client.put(
         '/users/0',
         json={
@@ -87,7 +93,7 @@ def test_update_user_not_found(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
